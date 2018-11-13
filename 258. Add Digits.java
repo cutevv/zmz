abbcedb34fa7557543ee77577058c36f213841bf
cutevv <44992399+cutevv@users.noreply.github.com>
2018-11-13 14:00:50 -0800

    Update 258. Add Digits.java

--- 258. Add Digits.java
+++ 258. Add Digits.java
@@ -1,5 +1,17 @@
 class Solution {
     public int addDigits(int num) {
-        return 1 + (num - 1) % 9;
+    	if (num< 10){
+    		return num;
+    	}
+    	
+   		while (num / 10 > 0){ // if the sum in the inner while loop still need to be add up 
+    		int sum = 0;
+    		while (num > 0){	//stop when reacing the most left digit
+    			sum += num % 10;
+    			num /= 10;
+    		}
+    		num = sum;
+    	}
+        return num;
     }
 }
