753dc9a0d0428eb81780e878510cca3f1959b78f
cutevv <44992399+cutevv@users.noreply.github.com>
2018-11-12 23:07:03 -0800

    Add files via upload

---
+++ 238. Product of Array Except Self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return nums;
+        }
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        //build left side product
+        for(int i = 1; i < nums.length; i++){
+            result[i] = result[i-1] * nums[i-1];
+        }
+        //build right side product
+        int right = 1;
+        for (int i = nums.length-1; i >=0; i--){
+            result[i] = right * result[i];
+            right = right * nums[i];
+        }
+        return result;
+    }
+}
+//time: O(n)
+//space: O(n)
