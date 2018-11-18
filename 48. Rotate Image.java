64668e2001059e1f425d9304b43816a2183e07ea
cutevv <nickchen1107@gmail.com>
2018-11-18 01:31:08 -0800

    48. Rotate Image

---
+++ 48. Rotate Image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        if(matrix == null || matrix.length == 0){
+            return;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        //first, swap with diagnoal direction 
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < i; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            } 
+        }
+        //secondly, swap from middle line, keep the middle column's element 
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < row / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][row -1 - j];
+                matrix[i][row - 1 - j] = temp;
+            }
+        }
+    }
+}
+
+//time: O(m * n) row* col
+//space: O(1)
