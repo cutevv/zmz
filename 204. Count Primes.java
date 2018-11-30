96897b7360d30f244bc3f2b2e5df20f75499025f
cutevv <nickchen1107@gmail.com>
2018-11-29 20:13:08 -0800

    204. Count Primes

---
+++ 204. Count Primes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] notPrime = new boolean[n];
+        for (int i = 2; i < n; i++){
+            if(notPrime[i] == false){
+                count++;
+            }
+            for (int j = 2; i * j < n ; j++){
+                notPrime[i * j] = true;
+            }
+        }
+        return count;   
+        
+    }
+}
+//time: O(n^2)
+//space: O(n)
